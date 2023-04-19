6439829e0e34e1d4aacaf189162f5fe223807b27
MikkoVasankari <vasankarimikko@gmail.com>
2023-04-19 12:43:30 +0300

    File extension now in list

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,73 @@
 use std::{ fs, io };
 use std::fs::DirBuilder;
 use std::ffi::OsStr;
-use std::path::{ Path, PathBuf };
+use std::path::{ PathBuf };
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 struct Cli {
-    /// The path to the file to read
+    /// The path to the directory to read from cli
     dir: PathBuf,
 }
 
 fn main() -> io::Result<()> {
     let args = Cli::parse();
-    println!("{:#?}", args);
 
-    // Creating new Directory
+    // Creating new Directories
     let mut path_txt = PathBuf::from(&args.dir);
     path_txt.push("texts");
-
-    //println!("{:#?} 1", path_txt);
     DirBuilder::new().recursive(true).create(path_txt).unwrap();
 
-    //let path = "texts";
-    //DirBuilder::new().recursive(true).create(path).unwrap();
+    let mut path_pics: PathBuf = PathBuf::from(&args.dir);
+    path_pics.push("pictures");
+    DirBuilder::new().recursive(true).create(path_pics).unwrap();
 
-    // Going through current Directory
+    // Going through given Directory
     let entries = fs
         ::read_dir(&args.dir)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
 
+    let vec_os_string: Vec<&OsStr> = vec![
+        OsStr::new("png"),
+        OsStr::new("jpg"),
+        OsStr::new("txt")
+    ];
+
     for item in &entries {
-        // Taking wanted file extension
-        let os_str_png = OsStr::new("png");
-        let os_str_jpg = OsStr::new("jpg");
 
-        let os_str_txt = OsStr::new("txt");
+        // Adding created directories to the path
 
-        // Adding jou/ Path to the files current Path
-        let location_path_pic = Path::new("pictures").join(item);
+        let path_txt = PathBuf::from(&args.dir);
+        let mut path_txt_os_string = path_txt.clone().into_os_string();
+        path_txt_os_string.push("/texts/");
 
-
-        let test_path_txt = PathBuf::from(&args.dir);
-
-        let mut test_os_string = test_path_txt.clone().into_os_string();
-
-        test_os_string.push("/texts/");
-        
-        //println!("{:#?}1", &test_os_string);
-        //  println!("{:#?}2", item);
-        // let location_path_txt = Path::new("texts").join(i);
-
-        //if i.extension() == Some(os_str_png) {
-        //   fs::rename(i, &location_path_pic)?;
-        //}
-
-        //if i.extension() == Some(os_str_jpg) {
-        //    fs::rename(i, &location_path_pic)?;
-        //}
+        let path_pics: PathBuf = PathBuf::from(&args.dir);
+        let mut path_pics_os_string = path_pics.clone().into_os_string();
+        path_pics_os_string.push("/pictures/");
 
 
-        // Tää toimii nyt missä vaan kansiossa :D 
-
-        if item.extension() == Some(os_str_txt) {
-
-            //println!("{:#?}", item.file_name().unwrap());
-            //println!("{:#?}", test_os_string);
-
-            test_os_string.push(item.file_name().unwrap());
-            fs::rename(item, test_os_string)?;
+        // Checking file extension and adding file to correct directory
+        if item.extension() == vec_os_string.get(0).copied() || item.extension() == vec_os_string.get(1).copied() {
+            path_pics_os_string.push(item.file_name().unwrap());
+            fs::rename(item, path_pics_os_string)?;
         }
 
-        // figurre out match in rust
-        //rintln!("{:#?}", i.extension());
-        //match i.extension() {
+        if item.extension() == vec_os_string.get(2).copied() {
+            path_txt_os_string.push(item.file_name().unwrap());
+            fs::rename(item, path_txt_os_string)?;
+        }
 
-        //None | Some(_) => todo!(),
+        // figure out match in rust
 
-        //Some(_) => println!("jou teksti!"),
+        //match item.extension() {
+        
 
-        //None => println!("this is a directory"),
+        //Some(_) if item.extension() == vec_os_string.get(0).copied() => {
+        //    println!("jou se o filu")
+        //},
+
+        //_ => println!("this is a directory"),
 
         //}
     }
