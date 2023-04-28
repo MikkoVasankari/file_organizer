15c1b5947205f8f91a0dc82f992b9f568639d714
MikkoVasankari <vasankarimikko@gmail.com>
2023-04-28 14:13:56 +0300

    Fixed issues when creating directories

--- README.md
+++ README.md
@@ -8,5 +8,4 @@ Should work with ```cargo run your/directory/path/here```
 
 ### TODO
     - Check file extension more efficiently.
-    - New directories created even if there is no files, Need to fix this.
     - Add more working file extensions.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{ fs, io };
 use std::fs::DirBuilder;
-use std::ffi::OsStr;
+use std::ffi::{OsStr, OsString};
 use std::path::{ PathBuf };
 use clap::Parser;
 
@@ -11,27 +11,14 @@ struct Cli {
 }
 
 fn main() -> io::Result<()> {
-
     let args = Cli::parse();
 
     if args.dir.is_dir() == true {
-        // Creating new Directories
-        let mut path_txt = PathBuf::from(&args.dir);
-        path_txt.push("texts");
-        DirBuilder::new().recursive(true).create(path_txt).unwrap();
-
-        let mut path_pics: PathBuf = PathBuf::from(&args.dir);
-        path_pics.push("pictures");
-        DirBuilder::new().recursive(true).create(path_pics).unwrap();
-
-        // Adding created directories to the path
-        let path_txt = PathBuf::from(&args.dir);
-        let mut path_txt_os_string = path_txt.clone().into_os_string();
-        path_txt_os_string.push("/texts/");
-
-        let path_pics: PathBuf = PathBuf::from(&args.dir);
-        let mut path_pics_os_string = path_pics.clone().into_os_string();
-        path_pics_os_string.push("/pictures/");
+        let vec_os_string: Vec<&OsStr> = vec![
+            OsStr::new("png"),
+            OsStr::new("jpg"),
+            OsStr::new("txt")
+        ];
 
         // Going through given Directory
         let entries = fs
@@ -39,13 +26,40 @@ fn main() -> io::Result<()> {
             .map(|res| res.map(|e| e.path()))
             .collect::<Result<Vec<_>, io::Error>>()?;
 
-        let vec_os_string: Vec<&OsStr> = vec![
-            OsStr::new("png"),
-            OsStr::new("jpg"),
-            OsStr::new("txt")
-        ];
-
+        // Checking if given directory has any files with wanted file extensions
         for item in &entries {
+            if
+                item.extension() == vec_os_string.get(0).copied() ||
+                item.extension() == vec_os_string.get(1).copied()
+            {   
+                let mut path_pics: PathBuf = PathBuf::from(&args.dir);
+                path_pics.push("pictures");
+                DirBuilder::new().recursive(true).create(path_pics).unwrap();
+            }
+
+            if item.extension() == vec_os_string.get(2).copied() {                
+                let mut path_txt = PathBuf::from(&args.dir);
+                path_txt.push("texts");
+                DirBuilder::new().recursive(true).create(path_txt).unwrap();
+            } 
+        }
+
+        // Adding created directories to the path
+        let path_pics: PathBuf = PathBuf::from(&args.dir);
+        let mut path_pics_os_string = path_pics.clone().into_os_string();
+        path_pics_os_string.push("/pictures/");
+        let path_pics_os_string_original:OsString = path_pics_os_string.clone();
+
+        let path_txt = PathBuf::from(&args.dir);
+        let mut path_txt_os_string = path_txt.clone().into_os_string();
+        path_txt_os_string.push("/texts/");
+        let path_txt_os_string_original:OsString = path_txt_os_string.clone();
+
+
+
+        // Inserting files to new paths
+        for item in &entries {
+
             // Checking file extension and adding file to correct directory
             if
                 item.extension() == vec_os_string.get(0).copied() ||
@@ -53,11 +67,25 @@ fn main() -> io::Result<()> {
             {
                 path_pics_os_string.push(item.file_name().unwrap());
                 fs::rename(item, path_pics_os_string.clone())?;
+                path_pics_os_string.clear();
+                path_pics_os_string.push(path_pics_os_string_original.clone());
+                println!(
+                    "Files was organized to directory {:?}",
+                    path_pics_os_string,
+                );
             }
+        
 
             if item.extension() == vec_os_string.get(2).copied() {
+                
                 path_txt_os_string.push(item.file_name().unwrap());
                 fs::rename(item, path_txt_os_string.clone())?;
+                path_txt_os_string.clear();
+                path_txt_os_string.push(path_txt_os_string_original.clone());
+                println!(
+                    "Files was organized to directory {:?}",
+                    path_txt_os_string,
+                );
             }
 
             // figure out match in rust
@@ -72,11 +100,7 @@ fn main() -> io::Result<()> {
 
             //}
         }
-        println!(
-            "Files are now organized to directories {:?} and {:?}",
-            path_pics_os_string,
-            path_txt_os_string
-        );
+
     } else {
         println!("Path you gave is not a directory");
     }
