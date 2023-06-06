098b5d9cbe317d9c49574657842edc1b5aed929b
MikkoVasankari <vasankarimikko@gmail.com>
2023-06-06 12:15:49 +0300

    Created functions for file and directory creating

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ File organizer to sort out files in given directory/folder.
 
 Currently organizes .txt / .png and .jpg files to appropriate directory to them.
 
-Should work with ```cargo run your/directory/path/here```
+Should work with ```cargo run /your/directory/path/here```
 
 
 ### TODO
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{ fs, io };
 use std::fs::DirBuilder;
-use std::ffi::{OsStr, OsString};
+use std::ffi::{ OsStr, OsString };
 use std::path::{ PathBuf };
 use clap::Parser;
 
@@ -26,81 +26,48 @@ fn main() -> io::Result<()> {
             .map(|res| res.map(|e| e.path()))
             .collect::<Result<Vec<_>, io::Error>>()?;
 
-        // Checking if given directory has any files with wanted file extensions
-        for item in &entries {
-            if
-                item.extension() == vec_os_string.get(0).copied() ||
-                item.extension() == vec_os_string.get(1).copied()
-            {   
-                let mut path_pics: PathBuf = PathBuf::from(&args.dir);
-                path_pics.push("pictures");
-                DirBuilder::new().recursive(true).create(path_pics).unwrap();
-            }
+        fn create_new_directory(path: PathBuf, ext: &OsStr) -> OsString {
+            let mut pathi_os_string = path.clone().into_os_string();
 
-            if item.extension() == vec_os_string.get(2).copied() {                
-                let mut path_txt = PathBuf::from(&args.dir);
-                path_txt.push("texts");
-                DirBuilder::new().recursive(true).create(path_txt).unwrap();
-            } 
+            pathi_os_string.push("/");
+
+            pathi_os_string.push(ext);
+
+            pathi_os_string.push("/");
+
+            DirBuilder::new().recursive(true).create(pathi_os_string.clone()).unwrap();
+
+            return pathi_os_string;
         }
 
-        // Adding created directories to the path
-        let path_pics: PathBuf = PathBuf::from(&args.dir);
-        let mut path_pics_os_string = path_pics.clone().into_os_string();
-        path_pics_os_string.push("/pictures/");
-        let path_pics_os_string_original:OsString = path_pics_os_string.clone();
-
-        let path_txt = PathBuf::from(&args.dir);
-        let mut path_txt_os_string = path_txt.clone().into_os_string();
-        path_txt_os_string.push("/texts/");
-        let path_txt_os_string_original:OsString = path_txt_os_string.clone();
-
-
-
-        // Inserting files to new paths
-        for item in &entries {
-
-            // Checking file extension and adding file to correct directory
-            if
-                item.extension() == vec_os_string.get(0).copied() ||
-                item.extension() == vec_os_string.get(1).copied()
-            {
-                path_pics_os_string.push(item.file_name().unwrap());
-                fs::rename(item, path_pics_os_string.clone())?;
-                path_pics_os_string.clear();
-                path_pics_os_string.push(path_pics_os_string_original.clone());
-                println!(
-                    "File was organized to directory {:?}",
-                    path_pics_os_string,
-                );
-            }
-        
-
-            if item.extension() == vec_os_string.get(2).copied() {
-                
-                path_txt_os_string.push(item.file_name().unwrap());
-                fs::rename(item, path_txt_os_string.clone())?;
-                path_txt_os_string.clear();
-                path_txt_os_string.push(path_txt_os_string_original.clone());
-                println!(
-                    "File was organized to directory {:?}",
-                    path_txt_os_string,
-                );
-            }
-
-            // figure out match in rust
-
-            //match item.extension() {
-
-            //Some(_) if item.extension() == vec_os_string.get(0).copied() => {
-            //    println!("jou se o filu")
-            //},
-
-            //_ => println!("this is a directory"),
-
-            //}
+        fn create_new_files(mut path: OsString, item: &PathBuf) {
+            path.push(item.file_name().unwrap());
+            fs::rename(item, path.clone()).expect("creating filed failed");
+            println!("File was organized to directory {:?}", path);
+            path.clear();
         }
 
+        for item in &entries {
+            if let Some(x) = item.extension() {
+                // x on nyt file extension
+
+                if Some(x) == vec_os_string.get(0).copied() {
+                    let path_os_string = create_new_directory(args.dir.clone(), x);
+                    create_new_files(path_os_string, item);
+                }
+
+                if Some(x) == vec_os_string.get(1).copied() {
+                    let path_os_string = create_new_directory(args.dir.clone(), x);
+                    create_new_files(path_os_string, item);
+                }
+
+                if Some(x) == vec_os_string.get(2).copied() {
+                    let path_os_string = create_new_directory(args.dir.clone(), x);
+                    create_new_files(path_os_string, item);
+                }
+
+            }
+        }
     } else {
         println!("Path you gave is not a directory");
     }
