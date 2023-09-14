eae687186c6327bae34e1d07be8cab010d096c2b
MikkoVasankari <vasankarimikko@gmail.com>
2023-09-14 08:49:26 +0300

    Modified code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{ fs, io, vec };
 use std::fs::DirBuilder;
 use std::ffi::{ OsStr, OsString };
-use std::path::PathBuf ;
+use std::path::PathBuf;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -13,8 +13,26 @@ struct Cli {
 fn main() -> io::Result<()> {
     let args = Cli::parse();
 
-    if args.dir.is_dir() == true {
+    fn create_new_directory(path: PathBuf, ext: &OsStr) -> OsString {
+        let mut pathi_os_string = path.clone().into_os_string();
 
+        pathi_os_string.push("/");
+        pathi_os_string.push(ext);
+        pathi_os_string.push("/");
+
+        DirBuilder::new().recursive(true).create(pathi_os_string.clone()).unwrap();
+
+        return pathi_os_string;
+    }
+
+    fn create_new_files(mut path: OsString, item: &PathBuf) {
+        path.push(item.file_name().unwrap());
+        fs::rename(item, path.clone()).expect("creating filed failed");
+        println!("File was organized to directory {:?}", path);
+        path.clear();
+    }
+
+    if args.dir.is_dir() == true {
         let vec_os_string: Vec<&OsStr> = vec![
             OsStr::new("png"),
             OsStr::new("jpg"),
@@ -26,29 +44,9 @@ fn main() -> io::Result<()> {
             .map(|res| res.map(|e| e.path()))
             .collect::<Result<Vec<_>, io::Error>>()?;
 
-        fn create_new_directory(path: PathBuf, ext: &OsStr) -> OsString {
-            let mut pathi_os_string = path.clone().into_os_string();
-
-            pathi_os_string.push("/");
-            pathi_os_string.push(ext);
-            pathi_os_string.push("/");
-
-            DirBuilder::new().recursive(true).create(pathi_os_string.clone()).unwrap();
-
-            return pathi_os_string;
-        }
-
-        fn create_new_files(mut path: OsString, item: &PathBuf) {
-            path.push(item.file_name().unwrap());
-            fs::rename(item, path.clone()).expect("creating filed failed");
-            println!("File was organized to directory {:?}", path);
-            path.clear();
-        }
-
         for item in &entries {
             if let Some(x) = item.extension() {
                 for i in 0..vec_os_string.len() {
-
                     if Some(x) == vec_os_string.get(i).copied() {
                         let path_os_string = create_new_directory(args.dir.clone(), x);
                         create_new_files(path_os_string, item);
